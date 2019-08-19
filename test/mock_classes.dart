407fe63d46ad08788d83d7434abe203d2c309beb
Lukasz Wisniewski <luke.cherrish@gmail.com>
2019-08-19 17:00:04 +0200

    support registering paths programmatically
    closes #6

--- test/mock_classes.dart
+++ test/mock_classes.dart
@@ -84,3 +84,18 @@ class MockHomeWidgetArgument2 extends StatelessWidget {
     );
   }
 }
+
+class CustomVoyager extends Voyager {
+  CustomVoyager(String path, Voyager parent)
+      : super(path: path, parent: parent, config: {});
+
+  set widget(WidgetBuilder builder) {
+    this[WidgetPlugin.KEY] = builder;
+  }
+
+  set title(String value) {
+    this["title"] = value;
+  }
+
+  String get title => this["title"];
+}
