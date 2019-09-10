862c8cfd5f7a9f64041e4c02b0ef1341bde79664
Lukasz Wisniewski <luke.cherrish@gmail.com>
2019-09-10 20:01:12 +0200

    more coverage as codecov is acting flakely

---
+++ test/codecov.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:voyager/voyager.dart';
+
+void main() {
+  test('static values because codecov complains', () {
+    expect(RouterNG.cupertinoRoute, RouterNG.cupertinoRoute);
+    expect(RouterNG.materialRoute, RouterNG.materialRoute);
+    expect(RedirectPlugin.KEY, RedirectPlugin.KEY);
+    expect(WidgetPlugin.KEY, WidgetPlugin.KEY);
+    expect(Voyager.KEY_TYPE, Voyager.KEY_TYPE);
+  });
+}
